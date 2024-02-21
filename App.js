be732b076c94acc4cefd7b6e495a9f72f872a767
Gabriel Donatti <125682008+gabrieldonatti99@users.noreply.github.com>
2024-02-21 13:45:52 -0300

    FirstApp

--- App.js
+++ App.js
@@ -2,19 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+return (
+    // começa view
+<View style={styles.container}>
+{/* os 3 textos */}
+<Text style={{fontWeight: 'bold',color: 'blue', fontSize: 30, textAlign: 'right', margin: 0,}}>Gabriel Camargo Donatti.</Text>
+
+<Text style={{fontStyle:'italic',color: 'black', fontSize: 25, textAlign: 'left', border: 'solid', borderWidth: 1, borderColor: 'black', margin: 0,}}>14.</Text>
+
+<Text style={{backgroundColor:'pink' ,color:'black', fontSize: 20, textAlign: 'center', margin: 0,}}>Sala:7</Text>
+
+<Text style={{color: 'black', textAlign: 'center', fontSize: 30, backgroundColor: 'red', border: 'solid', borderWidth: 1, borderColor: 'gold'}}>SENAI SUMARÉ.</Text>
+
+
+</View>
+)};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+
+flex: 1,
+justifyContent: 'center',
+margin: 0,
+display: 'flex',
+},
 });
